db2cd05c29ca90266d69d9b277657c6b2e514ab2
ArturKutlukaev <kutlukayev@bk.ru>
2024-06-28 23:50:38 +0500

    first commit

--- src/test/java/ru/netology/selenide/AppCardDeliveryTaskOnetest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOnetest.java
@@ -1,49 +1,49 @@
 package ru.netology.selenide;
 
-import com.codebrone.selenide.Condition;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openga.selenium.Keys;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.locks.Condition;
 
-import static com.codeborne.selenide.Selenide.*;
-
-public class AppCardDeliveryTaskOneTest {
-
-    private String generation;
-
-    private String generationate(long addDays, String pattern) {
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
-        return LocalDate.now().plusDay(addDays).format(DateTimeFormatter.ofPattern(pattern));
+class RegistrationTest {
+    @Test
+    void shouldRegisterByAccountNumberDOModification() {
+        open("http://localhost:9999");
+        $("[data-test-id='city']input").setValue("Казань");
+        String planninDate = generateDate(4, "dd.MM.yy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date')input").setValue(planninDate);
+        $("[data-test-id='name'] input").setValue("Иванов-Иваныч-Иван");
+        $("[data-test-id='phone'] input").setValue("+79991309695");
+        $("[data-test-id='agrement']").click();
+        $("button.button").click();
+        $("notification__content");
+        shouldRegisterByAccountNumberDOModification(Condition.visible, Duration.ofSeconds(20));
+        shouldRegisterByAccountNumberDOModification(Condition.exactText("Встреча успешно забронирована на" + planninDate));
+    }
 
+    private String generateDate(int i, String s) {
+
+        return s;
+    }
+
+    private void shouldRegisterByAccountNumberDOModification(Condition condition) {
+    }
+
+    private void shouldRegisterByAccountNumberDOModification(Condition visible, Duration duration) {
     }
 
 
-    class RegistrationTest {
-        @Test
-        void shouldRegisterByAccountNumberDOMModification() {
-            open  ("http://localhost:9999");
-            $("[data-test-id='city']input").setValue("Рязань");
-            String planningDate = generateDate( 4, "dd.MM.yy");
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-            $("[data-test-id='name'] input").setValue("Иванов-Иваныч-Иван");
-            $("[data-test-id='phone'] input").setValue("+79991309695");
-            $("[data-test-id='agrement']").click();
-            $("button.button").click();
-            $("notification__content");
-        .shouldRe(Condition. Duration.ofSeconds(15));
-        .shouldHave(Condition.exactText("Встреча успешно забронирована на" + planninDate));
-
-
-        }
-
-    }
 }
+
+
+
+
 
 
 
